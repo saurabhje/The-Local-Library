af6fca93c958cc81d58edb74435ee0245b833439
saurabhje <saurabhjesingh@gmail.com>
2023-09-07 23:20:27 +0530

    routing and controllers

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ async function main(){
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
