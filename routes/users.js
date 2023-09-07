251d9cc8f0a785d2eaf7980d929a5498b0bc9016
saurabhje <saurabhjesingh@gmail.com>
2023-09-07 15:47:59 +0530

    m

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 
 
-router.get('/', function(req, res, next) {
+router.get('/user', function(req, res, next) {
   res.send('respond with a resource');
 });
 
