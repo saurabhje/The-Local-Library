bb8c73feb76f6cd739fbc2497640182474f2977c
saurabhje <saurabhjesingh@gmail.com>
2023-09-09 21:44:03 +0530

    Due date formmated

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -17,6 +18,10 @@ const BookInstanceSchema = new Schema({
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual("url").get(function () {
   return `/catalog/bookinstance/${this._id}`;
+});
+
+BookInstanceSchema.virtual("due_back_formatted").get(function (){
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
