35e6f69c508bcaec15e6d5f28be523299e3d7cae
saurabhje <saurabhjesingh@gmail.com>
2023-09-10 17:45:01 +0530

    added author detail page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,7 +3,7 @@ const Book = require("../models/book");
 
 const asyncHandler = require("express-async-handler");
 
-// Display list of all Authors.
+
 exports.author_list = asyncHandler(async (req, res, next) => {
   const allAuthors = await Author.find().sort({ family_name: 1 }).exec();
   res.render("author_list", {
@@ -14,23 +14,24 @@ exports.author_list = asyncHandler(async (req, res, next) => {
 
 
 exports.author_detail = asyncHandler(async (req, res, next) => {
-  const [author, allBooksByAuthor] = await Promise.all ([
+  const [author, allBooksByAuthor] = await Promise.all([
     Author.findById(req.params.id).exec(),
-    Book.find({author: req.params.id}, "title summary").exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
   ]);
 
-  if(author === null){
-    const err = new Error("No such author Found");
+  if (author === null) {
+    const err = new Error("Author not found");
     err.status = 404;
     return next(err);
-  };
+  }
 
   res.render("author_detail", {
-    title: "Author Details",
+    title: "Author Detail",
     author: author,
     author_books: allBooksByAuthor,
-  })
+  });
 });
+
 
 exports.author_create_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author create GET");
--- models/author.js
+++ models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { DateTime } = require("luxon");
+const { DateTime } = require("luxon"); 
 
 const Schema = mongoose.Schema;
 
@@ -12,19 +12,37 @@ const AuthorSchema = new Schema({
 
 
 AuthorSchema.virtual("name").get(function () {
-    let fullname = "";
-    if (this.first_name && this.family_name) {
-      fullname = `${this.family_name}, ${this.first_name}`;
-    }
-  
-    return fullname;
+  return this.family_name + ", " + this.first_name;
 });
+
 
 AuthorSchema.virtual("url").get(function () {
-    return `catalog/author/${this._id}`
-})
-
-AuthorSchema.virtual("formattedDOB").get(function (){
-  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+  return `/catalog/author/${this._id}`;
 });
+
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  return lifetime_string;
+});
+
+AuthorSchema.virtual("formattedDOB").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); 
+});
+
+AuthorSchema.virtual("formattedDOD").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); 
+});
+
+
 module.exports = mongoose.model("Author", AuthorSchema);
