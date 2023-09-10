8094916b22a0c8eea94ad91fab6fad8125626fe6
saurabhje <saurabhjesingh@gmail.com>
2023-09-10 16:01:37 +0530

    added genre list

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,13 +1,12 @@
 const Genre = require("../models/genre");
 const asyncHandler = require("express-async-handler");
 
-// Display list of all Genre.
-exports.genre_list = asyncHandler(async (req, res, next) => {
-  const allGenres = await Genre.find().sort({ name: 1 }).exec();
 
+exports.genre_list = asyncHandler(async (req, res, next) => {
+  const genreList = await Genre.find().sort({name:1}).exec();
   res.render("genre_list", {
-    title: "Genre List",
-    genre_list: allGenres,
+    title: "All Genres",
+    genre_list: genreList,
   })
 });
 
