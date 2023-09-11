d7a8abeab6b0d67314e03e53059b34ab2b0aa294
saurabhje <saurabhjesingh@gmail.com>
2023-09-11 20:04:54 +0530

    adding the author form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,6 +2,7 @@ const Author = require("../models/author");
 const Book = require("../models/book");
 
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 
 exports.author_list = asyncHandler(async (req, res, next) => {
@@ -33,13 +34,61 @@ exports.author_detail = asyncHandler(async (req, res, next) => {
 });
 
 
-exports.author_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
-});
+exports.author_create_get = (req, res, next) => {
+  res.render("author_form", { title: "Create Author" });
+};
 
-exports.author_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-});
+
+exports.author_create_post = [
+  body("first_name")
+    .trim()
+    .isLength({min: 1})
+    .escape()
+    .withMessage("First name must be specified")
+    .isAlphanumeric()
+    .withMessage("Name contains non-alpahnumeric characters"),
+
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  
+  asyncHandler( async(req,res,next) => {
+    const errors = req.validationResult();
+
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    if(!errors.isEmpty()){
+      res.render("author_form", {
+        title: "Add a new Author",
+        author: author,
+        errors:  errors.array(),
+      });
+      return;
+    } else{      //not checking for duplicate authors in this Form, cause we can have authors with the same
+      await author.save();
+      res.redirect(author.url);
+    }
+  }),
+];
 
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author delete GET");
