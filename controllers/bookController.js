17aef13442160ed4a93e5d3a36fcf2b86c086111
saurabhje <saurabhjesingh@gmail.com>
2023-09-14 12:13:30 +0530

    added update method for books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -182,66 +182,69 @@ exports.book_delete_post = asyncHandler(async (req, res, next) => {
 
 
 exports.book_update_get = asyncHandler(async (req, res, next) => {
-  const [ book, author, allgenres ] = await Promise.all([
+  const [book, allAuthors, allGenres] = await Promise.all([
     Book.findById(req.params.id).populate("author").populate("genre").exec(),
     Author.find().exec(),
-    Genre.find().exec()
+    Genre.find().exec(),
   ]);
 
-  if(book == null ){
+  if (book === null) {
     const err = new Error("Book not found");
     err.status = 404;
-    next(err);
+    return next(err);
   }
 
-  for (const genre of allgenres) {
-    if (book.genre.indexOf(genre._id) > -1) {
-      genre.checked = "true";
+  for (const genre of allGenres) {
+    for (const book_g of book.genre) {
+      if (genre._id.toString() === book_g._id.toString()) {
+        genre.checked = "true";
+      }
     }
   }
 
   res.render("book_form", {
     title: "Update Book",
-    authors: author,
-    genres: allgenres,
-    book: book
+    authors: allAuthors,
+    genres: allGenres,
+    book: book,
   });
 });
 
 
-exports.book_update_post = () => [
-  (req,res,next) => {
-    if(!req.body.genre instanceof array){
-      if(typeof req.body.genre == undefined){
-        req.body.genre = []
-      } else{
+
+// Handle book update on POST.
+exports.book_update_post = [
+  // Convert the genre to an array.
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre === "undefined") {
+        req.body.genre = [];
+      } else {
         req.body.genre = new Array(req.body.genre);
       }
     }
     next();
   },
 
-  body("title", "Title must be specified")
-  .trim()
-  .isLength({min: 1})
-  .escape(),
-
-  body("summary", "Summary of the book must be provided")
-  .trim()
-  .isLength({min: 10})
-  .escape(),
-
+  // Validate and sanitize fields.
+  body("title", "Title must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
   body("author", "Author must not be empty.")
-  .trim()
-  .isLength({ min: 1 })
-  .escape(),
-
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("summary", "Summary must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
   body("isbn", "ISBN must not be empty").trim().isLength({ min: 1 }).escape(),
   body("genre.*").escape(),
 
   asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
 
+    const errors = validationResult(req);
 
     const book = new Book({
       title: req.body.title,
@@ -249,16 +252,17 @@ exports.book_update_post = () => [
       summary: req.body.summary,
       isbn: req.body.isbn,
       genre: typeof req.body.genre === "undefined" ? [] : req.body.genre,
-      _id: req.params.id,
+      _id: req.params.id, 
     });
 
     if (!errors.isEmpty()) {
-      const [allAuthors, allgenres] = await Promise.all([
+
+      const [allAuthors, allGenres] = await Promise.all([
         Author.find().exec(),
         Genre.find().exec(),
       ]);
 
-      for (const genre of allgenres) {
+      for (const genre of allGenres) {
         if (book.genre.indexOf(genre._id) > -1) {
           genre.checked = "true";
         }
@@ -266,7 +270,7 @@ exports.book_update_post = () => [
       res.render("book_form", {
         title: "Update Book",
         authors: allAuthors,
-        genres: allgenres,
+        genres: allGenres,
         book: book,
         errors: errors.array(),
       });
