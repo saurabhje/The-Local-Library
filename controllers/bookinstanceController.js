d3538d2a406447cb60ae47d63e60eea96902b57a
saurabhje <saurabhjesingh@gmail.com>
2023-09-16 17:13:07 +0530

    updating the form

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -85,27 +85,87 @@ exports.bookinstance_create_post = [
 ]
 
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  const bookinstance = await BookInstance.findById("req.params.id").exec();
-  if(bookinstance == null ){
+  const bookInstance = await BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec();
+
+  if (bookInstance === null) {
     res.redirect("/catalog/bookinstances");
   }
-  res.render("bookinstance_delete", { 
-    title: "Delete Book Instance",
-    bookinstance: bookinstance,
+
+  res.render("bookinstance_delete", {
+    title: "Delete BookInstance",
+    bookinstance: bookInstance,
   });
 });
 
 
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  
+  await BookInstance.findByIdAndRemove(req.body.id);
+  res.redirect("/catalog/bookinstances");
 });
 
 
 exports.bookinstance_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance update GET");
+  const [bookinstance, book] = await Promise.all([
+    await BookInstance.findById(req.params.id).populate("book").exec(),
+    Book.find().exec(),
+  ]);
+
+  if(bookinstance == null){
+    const err = new Error("No such bookinstance");
+    err.status = 404;
+    next(err);
+  }
+
+  res.render("bookinstance_form", {
+    title: "Update BookInstance",
+    selected_book: bookinstance.book._id,
+    book_list : book,
+    bookinstance : bookinstance,
+  });
 });
 
+exports.bookinstance_update_post = [
+  body("book", "Book must be specified").trim().isLength({min: 1}).escape(),
 
-exports.bookinstance_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance update POST");
-});
+  body("imprint" ,"Imprint must be specified")
+  .trim()
+  .isLength({min: 1})
+  .escape(),
+
+  body("status").escape(),
+
+  body("due_back", "Invalid date")
+  .optional({values: "falsy"})
+  .isISO8601(),
+
+  asyncHandler( async(req,res,next ) => {
+
+    const errors = validationResult(req)
+
+    const bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint : req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id: req.params.id,
+    });
+
+    if(!errors.isEmpty()){
+      const allBooks = await Book.find({}, "title").exec();
+
+      res.render("bookinstance_form", {
+        title: "Update bookinstance",
+        book_list: allBooks,
+        selected_book : BookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookinstance,
+      });
+      return;
+    } else{
+      await BookInstance.findByIdAndUpdate(req.params.id, bookinstance)
+      res.redirect(bookInstance.url);
+    }
+  })
+];
