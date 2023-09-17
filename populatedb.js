466c996f59660194ab3573a73398c427b11a5d5f
saurabhje <saurabhjesingh@gmail.com>
2023-09-17 19:23:35 +0530

    kremlin

--- populatedb.js
+++ populatedb.js
@@ -1,10 +1,8 @@
-#! /usr/bin/env node
-
 console.log(
   'This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
 );
 
-// Get arguments passed on command line
+
 const userArgs = process.argv.slice(2);
 
 const Book = require("./models/book");
