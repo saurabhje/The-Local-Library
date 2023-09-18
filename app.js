4607a77a237339a0e30c3f5d29e57edef8787d8d
saurabhje <saurabhjesingh@gmail.com>
2023-09-18 19:17:37 +0530

    added hamlet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require("compression");
+const helmet = require("helmet");
 require('dotenv').config();
 
 const mongoose = require('mongoose');
@@ -24,14 +25,23 @@ const catalogRouter = require('./routes/catalog');
 const app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
