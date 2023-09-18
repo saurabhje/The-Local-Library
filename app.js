f4120679f7a25ed799dc5a65a1932326045611cd
saurabhje <saurabhjesingh@gmail.com>
2023-09-18 23:39:57 +0530

    first_name

--- app.js
+++ app.js
@@ -18,13 +18,17 @@ async function main(){
   await mongoose.connect(mongoDB);
 }
 
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');
 
 const app = express();
-
-// view engine setup
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -35,6 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
+app.use(limiter);
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
