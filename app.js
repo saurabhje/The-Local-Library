342c133539b1cbbf3cd3940286e5cbb0c594c421
saurabhje <saurabhjesingh@gmail.com>
2023-09-18 19:13:57 +0530

    added compression

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require("compression");
 require('dotenv').config();
 
 const mongoose = require('mongoose');
@@ -30,6 +31,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
