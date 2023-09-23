4aa5fa205f9f8407ea421ae39ff44820f662d0eb
saurabhje <saurabhjesingh@gmail.com>
2023-09-23 18:25:14 +0530

    wow

--- routes/user.js
+++ routes/user.js
@@ -7,8 +7,8 @@ router.get("/create",  userController.createUser_get);
 
 router.post("/create", userController.createUser_post);
 
-router.get("login", userController.loginUser_get);
+router.get("/login", userController.loginUser_get);
 
-router.post("login", userController.loginUser_post);
+router.post("/login", userController.loginUser_post);
 
 module.exports = router;
