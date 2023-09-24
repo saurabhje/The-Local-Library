5748b0322d2c75b3ff0db7092e81910a47946814
saurabhje <saurabhjesingh@gmail.com>
2023-09-24 11:05:05 +0530

    logged

--- models/book.js
+++ models/book.js
@@ -4,13 +4,13 @@ const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: Schema.Types.ObjectId, ref: "Author", required: true }, //refrence to the Author
+  author: { type: Schema.ObjectId, ref: "Author", required: true },
   summary: { type: String, required: true },
   isbn: { type: String, required: true },
-  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],   //refrence to the genre
+  genre: [{ type: Schema.ObjectId, ref: "Genre" }],
 });
 
-// Virtual for book's URL
+
 BookSchema.virtual("url").get(function () {
   return `/catalog/book/${this._id}`;
 });
